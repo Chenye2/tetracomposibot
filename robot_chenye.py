06911e91f37589a4caa7b2401c4d9bb9b37cb616
Chenye <llchenye341202@gmail.com>
2026-02-07 23:57:28 +0100

    robot 0 de chenye

--- robot_chenye.py
+++ robot_chenye.py
@@ -22,47 +22,17 @@ class Robot_player(Robot):
     def step(self, sensors, sensor_view=None, sensor_robot=None, sensor_team=None):
         
         front = sensors[sensor_front]
-        left = sensors[sensor_front_left]
-        right = sensors[sensor_front_right]
+        front_left = sensors[sensor_front_left]
+        front_right = sensors[sensor_front_right]
+        left = sensors[sensor_left]
+        right = sensors[sensor_right]
 
-        # robot 0 : prendre maximum de cases (braitenberg avoider)
-        if self.robot_id == 0:
-            # niveau 1 
-            if front < 0.5 or left < 0.4 or right < 0.4:
-                if self.memory >= 0:
-                    self.memory += 1
-                    print("MEMEORY ROBOT 0 :", self.memory)
-                else:
-                    if self.memory >= 0:
-                        self.memory = 0
-
-                # déclenchement du mode échappement
-                if self.memory > 5:
-                    self.memory = -10   # 10 steps d'échappement
-
-                # mode échappement actif
-                if self.memory < 0:
-                    self.memory += 1
-                    print("MEMEORY ECHAPPEMENT ROBOT 0 :", self.memory)
-                    translation = 0.1
-                    if left < right:
-                        rotation = -0.7
-                    else:
-                        rotation = 0.7
-                
-            
-            # niveau 2 : comportement Braitenberg avoider
-            if front < 0.8 or left < 0.8 or right < 0.8:
-                translation = front
-                rotation = 0.5 * (1 - sensors[sensor_front]) + 0.5 * (sensors[sensor_front_left] - sensors[sensor_front_right]) + (random.random()-0.5)*0.15
-
-            # niveau 3 : exploration
-            else:
-                translation = front*0.6
-                rotation = (random.random() - 0.5) * 0.3
+        # robot 0 : braitenberg avoider + subsomption (ou po)
+        if self.robot_id == 0 or self.robot_id == 1 or self.robot_id == 2 or self.robot_id == 3:
+            translation = front*0.3 + 0.7
+            rotation = 0.3*(1-front) + 0.4*(front_left - front_right) + 0.3*(left - right) + (random.random()-0.5)*0.2
 
 
-        
         # robot 1 : braitenberg + subsomption (si trop longtemps sans avancer : forcer tourner)
         # elif self.robot_id == 1:
             
