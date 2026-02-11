a057c5dd5fdc13222be6cfe701730f8dbe88a403
loiszhu <130663488+loiszhu@users.noreply.github.com>
2026-02-11 02:43:19 +0100

    jnmr

--- robot_lolo.py
+++ robot_lolo.py
@@ -66,64 +66,69 @@ class Robot_player(Robot):
 
         # robot 1 : robot stalker (braitenberg loveBot)
         if self.robot_id == 0 or self.robot_id == 2 or self.robot_id == 1 or self.robot_id == 3 :
-            # Mode d’échappement : si mur ou robot très proche
-            if self.memory < 0:
-                self.memory += 1
-                # translation minime pour que le robot puisse tourner
-                translation = 0.05
-
-                # si la distance entre la droite et la gauche pareille, tourne random
-                if abs(left - right) < 0.05:
-                    rotation = random.choice([-1, 1])
-                # sinon, tourne vers là où il peut
+        
+            # Mode débloquage : activé si bloqué depuis 5 steps
+            if self.memory >= 5:
+                self.memory = 0  # reset
+                translation = -sensors[sensor_front]  # reculer
+                if left < right:
+                    rotation = -1 
                 else:
-                    if left < right:
-                        rotation = -1
-                    else:
-                        rotation = 1
-
+                    rotation = 1
                 return translation, rotation, False
-            
+
             # détection d'obstacle
-            if front < 0.5:
+            if front < 0.4 or front_left < 0.3 or front_right < 0.3:
                 self.memory += 1
             else :
                 self.memory = max(0, self.memory - 1)
-            
-            # si bloqué trop longtemps, activation déblocage
-            if self.memory > 5:
-                self.memory = -10  # activer mode déblocage 10 steps
+
 
             # Niveau 2 : détection de robot adverse, le suivre
             # si robot détecté devant, devant à gauche ou devant à droite ET si c'est robot de l'équipe adverse
-            elif ((sensor_to_robot[sensor_front] < 0.8 and sensor_team[sensor_front] != self.team_name) or
+            if ((sensor_to_robot[sensor_front] < 0.8 and sensor_team[sensor_front] != self.team_name) or
                   (sensor_to_robot[sensor_front_left] < 0.8 and sensor_team[sensor_front_left] != self.team_name) or
                   (sensor_to_robot[sensor_front_right] < 0.8 and sensor_team[sensor_front_right] != self.team_name)):
-                
-                # réinitialise memory à zéro
-                self.memory = 0
+
                 # avance lentement vers le robot ennemi détecté
-                translation = sensor_to_robot[sensor_front] * 0.3 + 0.2 * (sensor_to_robot[sensor_front_left] + sensor_to_robot[sensor_front_right])
+                translation = sensor_to_robot[sensor_front] * 0.4 + 0.2
                 # tourne vers le robot ennemi détecté
-                rotation = (sensor_to_robot[sensor_front_right] - sensor_to_robot[sensor_front_left]) + 0.6 * (sensor_to_robot[sensor_right] - sensor_to_robot[sensor_left])
+                rotation = ((sensor_to_robot[sensor_front_right] - 
+                             sensor_to_robot[sensor_front_left]) + 
+                            (sensor_to_robot[sensor_right] - 
+                             sensor_to_robot[sensor_left]) +
+                            (sensor_to_robot[sensor_rear_right] -
+                             sensor_to_robot[sensor_rear_left]))
+                
+                return translation, rotation, False
             
             # Niveau 3 : détection d’obstacle, l’éviter
-            elif front < 0.8 or left < 0.6 or right < 0.6:
-                # réinitialise memory à zéro
-                self.memory = 0
+            elif front < 0.6 or left < 0.6 or right < 0.6:
 
                 # avance en fonction de la distance entre l'obstacle et le robot
-                translation = front*0.3 + 0.7
-                
+                translation = front*0.3 + 0.4
                 # tourne à l'opposé de l'obstacle avec une petite déviation aléatoire
-                rotation = 0.3*(1-front) + 0.5*(front_left - front_right) + 0.3*(left - right) + (random.random()-0.5)*0.2
+                #rotation = 0.8*(front_left - front_right) + 0.3*(left - right) + (random.random()-0.5)*0.15
+                
+                rotation = (
+                    sensors[sensor_front_left] +
+                    sensors[sensor_left] +
+                    sensors[sensor_rear_left] -
+                    sensors[sensor_front_right] -
+                    sensors[sensor_right] -
+                    sensors[sensor_rear_right] +
+                    random.uniform(-0.5, 0.5)
+                )
+                
+                return translation, rotation, False
+            
+            
             
             # Niveau 4 : comportement par défaut, l’exploration
-            else:
-                self.memory = 0
-                # vitesse fixe
-                translation = 1
-                # déviation aléatoire
-                rotation = (random.random() - 0.5) * 0.3
+
+            # vitesse fixe
+            translation = 1
+            # déviation aléatoire
+            rotation = (random.random() - 0.5) * 0.3
 
         return translation, rotation, False
